6f822c85c2599e5479f4719a8d72f1c6a8282aba
sfilipt <filip.strand@hotmail.com>
2023-11-20 11:16:46 +0100

    Färdigt program
    Gjort klart switchen och lagt till avslutningsmeddelande.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@
         {
             int menuChoice = 0;
             int numberOfFamilyMembers = 0;
-            int[] familyAges = Array.Empty<int>();
+            double sumOfAges = 0;
+            double meanOfAges = 0;
+            double[] familyAges = Array.Empty<double>();
             string[] familyNames = Array.Empty <string>();
             while (menuChoice != 5)
             {
@@ -16,7 +18,7 @@
                     "2. Skriv ut namn på alla familjemedlemmar som du angivit. \n" +
                     "3. Skriv ut summan av åldrarna för de familjemedlemmar som du angivit. \n" +
                     "4. Skriv ut medelåldern för de familjemedlemmar som du angivit. \n" +
-                    "5. Avluta programmet");
+                    "5. Avluta programmet \n---------------------------------------------------------------\n");
                 menuChoice = int.Parse(Console.ReadLine());
                 {
                     switch (menuChoice)
@@ -25,7 +27,7 @@
                         case 1:
                             Console.WriteLine("Hur många familjemedlemmar vill du mata in i programmet?");
                             numberOfFamilyMembers = int.Parse(Console.ReadLine());
-                            familyAges = new int[numberOfFamilyMembers];
+                            familyAges = new double[numberOfFamilyMembers];
                             familyNames = new string[numberOfFamilyMembers];
                                 for (int i = 0; i < numberOfFamilyMembers; i++)
                                 {
@@ -33,26 +35,50 @@
                                     familyNames[i] = Console.ReadLine();
                                     Console.WriteLine(familyNames.Length);
                                     Console.WriteLine($"Hur gammal är {familyNames[i]}?");
-                                    familyAges[i] = int.Parse(Console.ReadLine());
+                                    familyAges[i] = Convert.ToDouble(Console.ReadLine());
                                 }
                             break;
                         case 2:
                             if (numberOfFamilyMembers == 0)
-                                Console.WriteLine("Du har inte lagt till några familjemedlemmar än. Välj 1 i menyn för att lägga till familjemedlemmar");
+                                Console.WriteLine("\n Du har inte lagt till några familjemedlemmar än. Välj 1 i menyn för att lägga till familjemedlemmar \n");
                             else
+                            {
                                 Console.WriteLine("Dessa familjemedlemmar har du lagt till i programmet: ");
                                 for (int i = 0; i < familyNames.Length; i++)
-                                Console.WriteLine($"{familyNames[i]} \n");
+                                    Console.WriteLine($"{familyNames[i]} \n");
+                            }
                             break;
                         case 3:
+                            if (numberOfFamilyMembers == 0)
+                                Console.WriteLine("\n Du har inte lagt till några familjemedlemmar än. Välj 1 i menyn för att lägga till familjemedlemmar \n");
+                            else
+                            {
+                                {
+                                    for (int i = 0; i < familyAges.Length; i++)
+                                        sumOfAges += familyAges[i];
+                                }
+                                Console.WriteLine($"\n Summan av de familjemedlemmar du har angivit är: {sumOfAges:#} \n");
+                            }
                             break;
                         case 4:
+                            if (numberOfFamilyMembers == 0)
+                                Console.WriteLine("\n Du har inte lagt till några familjemedlemmar än. Välj 1 i menyn för att lägga till familjemedlemmar \n");
+                            else
+                            {
+                                {
+                                    for (int i = 0; i < familyAges.Length; i++)
+                                        meanOfAges += familyAges[i];
+                                }
+                                Console.WriteLine($"\n Medelåldern för de familjemedlemmar du har angivit är: {meanOfAges / numberOfFamilyMembers:#.##}");
+                            }
                             break;
-                        case 5:
-                            System.Environment.Exit("Programmet avslutas nu.");
                         }
                     }
                 }
+            if (menuChoice == 5)
+            {
+                Console.WriteLine("\n Programmet avslutas nu. \n");
+            }
             }
         }
     }
